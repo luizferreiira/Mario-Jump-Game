25765c66b95cce9b29cc9d31ae81b448bef01104
luizferreiira <luizferreiraa34@gmail.com>
2022-07-31 06:41:07 -0300

    Mario Jump Game

---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const clouds = document.querySelector('.clouds');
+const gameOver = document.querySelector('.game-over');
+const gameBoard = document.querySelector('.game-board');
+
+const jump = () =>{
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+
+        mario.classList.remove('jump');
+
+    },500);
+}
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const cloudsPosition = clouds.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = './images/game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+
+        clouds.style.animation = 'none';
+        clouds.style.left = `${cloudsPosition}px`;
+
+        gameOver.innerHTML = "GAME OVER";
+        
+        gameBoard.classList.remove('game-board');
+        gameBoard.classList.add('game-board-dark');
+
+
+        clearInterval(loop);
+
+    }
+
+}, 10)
+
+document.addEventListener('keydown', jump);
